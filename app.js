f873d791cf8cfa11215ca423aace3c6a15e127d3
wenfei.li <wenfei.li@ectrip.com>
2021-01-22 18:02:20 +0800

    saas--龙凤山庄初版

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ App({
   onLaunch(options) {
     // 第一次打开
     // options.query == {number:1}
-    console.info(options, 'App onLaunch');
+    console.info('App onLaunch');
   },
   onShow(options) {
     // 从后台被 scheme 重新打开
